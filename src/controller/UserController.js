37dff2b12cc65a136ec7e6af9fb7c0a26204407b
Frederico Graziani <frederico.graz@gmail.com>
2024-07-04 16:58:34 -0300

    Criado o controller, routes e migration de user

---
+++ src/controller/UserController.js
@@ -0,0 +1,36 @@
+const { hash, compare } = require("bcryptjs");
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
+class UserController {
+  // Método de criar um novo Usuario
+  async create(req, res) {
+    try {
+      let { name, email, password } = req.body;
+      const allRequiredDataAvailable = name && email && password;
+
+      if (!allRequiredDataAvailable) {
+        throw new AppError("Por favor preencha todos os campos necessários!");
+      }
+
+      email = email.toLowerCase();
+
+      const user = await knex("users").where({ email }).first();
+
+      //Se o email já existe
+      if (user) throw new AppError("Email já cadastrado");
+      else {
+        const hashedPassword = await hash(password, 10);
+
+        //Insere um novo usuário
+        await knex("users").insert({ name, email, password: hashedPassword });
+
+        res.status(201).json();
+      }
+    } catch (error) {
+      throw new AppError(error.message);
+    }
+  }
+}
+
+module.exports = UserController;
