bff075734f582dbd20b2dc6867a44adcf962534f
Frederico Graziani <frederico.graz@gmail.com>
2024-07-05 18:19:25 -0300

    Adicionado rotas de expenses + incomes + budget

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
 const { Router } = require("express");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 // Todas as rotas
 const userRoutes = require("./users.router");
 const sessionsRoutes = require("./sessions.router");
+const expensesRoutes = require("./expenses.router");
+const incomesRoutes = require("./incomes.router");
+const budgetRoutes = require("./budget.router");
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/sessions", sessionsRoutes);
+routes.use("/expenses", ensureAuthenticated, expensesRoutes);
+routes.use("/incomes", ensureAuthenticated, incomesRoutes);
+routes.use("/budget", ensureAuthenticated, budgetRoutes);
 
 module.exports = routes;
