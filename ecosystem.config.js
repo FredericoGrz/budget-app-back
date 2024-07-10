a34fd426feec9326cd1ace0cacf858ea2371e3c3
Frederico Graziani <frederico.graz@gmail.com>
2024-07-10 14:25:28 -0300

    Carrega as varias ambientes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,22 @@
+require("dotenv").config();
 module.exports = {
   apps: [
     {
       name: "app",
       script: "./src/server.js",
       instances: "max",
+      exec_mode: "cluster",
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: process.env.ENVIRONMENT,
+        PORT: process.env.PORT,
+        DATABASE_URL: process.env.DB_CONN,
+        AUTH_SECRET: process.env.AUTH_SECRET,
       },
       env_production: {
-        NODE_ENV: "production",
+        NODE_ENV: process.env.ENVIRONMENT,
+        PORT: process.env.PORT,
+        DATABASE_URL: process.env.DB_CONN,
+        AUTH_SECRET: process.env.AUTH_SECRET,
       },
     },
   ],
